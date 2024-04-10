6274b1276c66b7dd99dbfb919a4f842b30eaae40
teddy7201 <henry10004@gmail.com>
2024-04-09 20:28:17 -0400

    Created Iterator Class

---
+++ COMP 228 Project 3/src/iterators/DDLIterator.java
@@ -0,0 +1,19 @@
+package iterators;
+
+import java.util.Iterator;
+
+public class DDLIterator<E> implements Iterator<E> {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
